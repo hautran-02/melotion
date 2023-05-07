8f959aaa28015cb08aadc59ccd7dc06fd845d725
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-05-07 09:46:38 +0700

    player page

---
+++ app/src/main/java/com/example/musicplayer/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.musicplayer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
